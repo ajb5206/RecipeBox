3410ad87ba12b0a837866545a49c0b7b2d3dfa55
Alex Bertotto <ajb5206@gmail.com>
2021-10-25 20:35:33 -0700

    add intial mvc template with text for work on recipe box user project
    Co-authored-by: Dylan Allison-Damitz <dylandamitz@gmail.com>

---
+++ Models/Recipe.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+namespace RecipeBox.Models
+{
+	public class Recipe
+	{
+
+		public Recipe()
+		{
+			this.JoinEntities = new HashSet<RecipeTag>();
+		}
+		public int RecipeId { get; set; }
+		public string RecipeName { get; set; }
+		public string Ingredients { get; set; }
+		
+		public virtual ICollection<RecipeTag> JoinEntities { get; }
+	}
+}
